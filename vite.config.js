c054d278d44a05379e888529d5dab439cd3e39e1
anurag721721 <anurag.yadav@aayaninfotech.com>
2025-04-28 16:04:04 +0530

    fixed lucide

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     hmr: {
-      overlay: false, // Disable HMR overlay for errors
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['lucide-react'], // If you want to externalize `lucide-react`
-    },
-  },
-  resolve: {
-    alias: {
-      // Add alias to fix potential import resolution issues
-      '@lucide/react': 'lucide-react',
+      overlay: false,
     },
   },
 })
